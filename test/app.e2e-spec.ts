4a9cb629599669c81e2686b6b44715a10c1a5bd8
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-07 13:52:01 +0100

    Test with auth token

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -110,15 +110,26 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
       });
     });
   });
 
-  // describe('User', () => {
-  //   describe('Get me', () => {});
-  //   describe('Edit user', () => {});
-  // });
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
+    // describe('Edit user', () => {});
+  });
 
   // describe('Bookmarks', () => {
   //   describe('Create bookmark', () => {});
